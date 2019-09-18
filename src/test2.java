e6226f4e489905a2b8a453a7147b75845b1d563d
ismetsean@gmail.com <Anadolu1071#>
2019-09-17 19:41:55 -0500

    lesson 1

--- src/test2.java
+++ src/test2.java
@@ -2,7 +2,9 @@ public class test2 {
     public static void main(String[] args)
     {
 
-        System.out.println("rest");
+        System.out.println("rest111");
         System.out.println("rest2");
+        System.out.println("rest3");
+
     }
 }
