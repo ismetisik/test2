82c9700dc246d14ccbda69df18fcf50cd05f8a5c
ismetsean@gmail.com <Anadolu1071#>
2019-09-17 19:39:35 -0500

    lesson 1

--- src/test2.java
+++ src/test2.java
@@ -1,5 +1,8 @@
 public class test2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         System.out.println("rest");
+        System.out.println("rest2");
     }
 }
